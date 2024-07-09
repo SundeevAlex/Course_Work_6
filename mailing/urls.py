49efb333a45f2670cf4cbd4558e30afcc6a9f931
Aleksandr Sundeev <navigator97@mail.ru>
2024-07-09 17:48:52 +0700

    Create client_list

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from mailing.apps import MailingConfig
-from mailing.views import index
+from mailing.views import HomeView, ClientListView
 
 app_name = MailingConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
+    # path('', index, name='index'),
+    path('', HomeView.as_view(), name='index'),
+    path('clients_list/', ClientListView.as_view(), name='clients_list'),
 ]
--- mailing/views.py
+++ mailing/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, CreateView, ListView
+from django.urls import reverse_lazy, reverse
+from mailing.models import Mailing, Client
 
 
-def index(request):
-    return render(request, 'mailing/index.html')
+# def index(request):
+#     return render(request, 'mailing/index.html')
+
+class HomeView(TemplateView):
+    """
+    Контроллер главной страницы сайта
+    """
+    template_name = 'mailing/index.html'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        mailings = Mailing.objects.all()
+        clients = Client.objects.all()
+        context_data['all_mailings'] = mailings.count()
+        context_data['active_mailings'] = mailings.filter(status=Mailing.STARTED).count()
+        context_data['active_clients'] = clients.values('email').distinct().count()
+
+        return context_data
+
+
+class ClientListView(ListView):
+    """
+    Контроллер отвечающий за отображение списка клиентов
+    """
+    model = Client
+
+    def get_queryset(self, queryset=None):
+        queryset = super().get_queryset()
+        user = self.request.user
+        # if not user.is_superuser and not user.groups.filter(name='manager'):
+        #     queryset = queryset.filter(owner=self.request.user)
+        return queryset
